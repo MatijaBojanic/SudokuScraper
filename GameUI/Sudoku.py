deee8c8a58d2a198ee89853aec17b557ffb07634
MatijaBojanic <matija.bojanic@gmail.com>
2022-04-03 17:21:09 +0200

    Refactored to make the code clear

--- GameUI/Sudoku.py
+++ GameUI/Sudoku.py
@@ -5,7 +5,7 @@ import numpy as np
 from GameUI.SudokuCell import SudokuCell
 
 
-# TODO-OPTIONAL: Auto solve button
+# This class represents the game itself. It generates the UI and contains checks for valid inputs
 class Sudoku:
     def __init__(self, sudoku_generator):
         self.sudoku_solution = None
@@ -15,108 +15,120 @@ class Sudoku:
         self.window = tk.Tk()
         self.window.maxsize(width=500, height=440)
         self.window.minsize(width=500, height=440)
-        self.make_grid()
+        self.new_game()
+        self.make_buttons()
         self.window.mainloop()
 
-    def make_grid(self):
+    # Grab another puzzle and make a grid for it
+    def new_game(self):
         self.input_fields = []
         sudoku_array = self.sudoku_generator.get_sudoku()
         self.sudoku_solution = sudoku_array
         self.current_state = sudoku_array
-        # we need to track current_state and from that deduce the allowed input values. Every time something is inputed
-        # we would need to update the list. And sudokuCells should always have acces to that list
+        self.make_grid()
 
-        for row in range(0, len(sudoku_array[0])):
-            x_cord = 0
-            for column in range(0, len(sudoku_array[row])):
+    # Creates the grid input fields
+    def make_grid(self):
+        for row in range(0, len(self.sudoku_solution[0])):
+            x_axis = 0
+            for column in range(0, len(self.sudoku_solution[row])):
                 if random.randint(0, 9) > 6:
-                    temp = SudokuCell(self, row, column, sudoku_array[row][column], self.window, justify='center')
+                    temp = SudokuCell(self, row, column, self.sudoku_solution[row][column], self.window)
                     self.input_fields.append(temp)
                     self.current_state[row][column] = ''
                 else:
-                    v = tk.StringVar(self.window, value=str(sudoku_array[row][column]))
-                    temp = tk.Entry(readonlybackground='#ADD8E6', textvariable=v, justify='center', state='readonly')
-                temp.place(x=x_cord, y=row * 40, width=40, height=40)
-                x_cord = x_cord + 40
+                    locked_field_value = tk.StringVar(self.window, value=str(self.sudoku_solution[row][column]))
+                    temp = tk.Entry(readonlybackground='#ADD8E6', textvariable=locked_field_value, justify='center',
+                                    state='readonly')
+                temp.place(x=x_axis, y=row * 40, width=40, height=40)
+                x_axis = x_axis + 40
 
-        reset_button = tk.Button(self.window, text="New Sudoku", command=self.make_grid)
-        submit_button = tk.Button(self.window, text="Submit", command=self.all_answers)
+    # Make UI buttons, for input, for new game, and for submitting results
+    def make_buttons(self):
+        reset_button = tk.Button(self.window, text="New Sudoku", command=self.new_game)
+        submit_button = tk.Button(self.window, text="Submit", command=self.win_check)
+        input_one = tk.Button(self.window, text="1", command=lambda: self.set_text("1", self.window.focus_get()))
+        input_two = tk.Button(self.window, text="2", command=lambda: self.set_text("2", self.window.focus_get()))
+        input_three = tk.Button(self.window, text="3", command=lambda: self.set_text("3", self.window.focus_get()))
+        input_four = tk.Button(self.window, text="4", command=lambda: self.set_text("4", self.window.focus_get()))
+        input_five = tk.Button(self.window, text="5", command=lambda: self.set_text("5", self.window.focus_get()))
+        input_six = tk.Button(self.window, text="6", command=lambda: self.set_text("6", self.window.focus_get()))
+        input_seven = tk.Button(self.window, text="7", command=lambda: self.set_text("7", self.window.focus_get()))
+        input_eight = tk.Button(self.window, text="8", command=lambda: self.set_text("8", self.window.focus_get()))
+        input_nine = tk.Button(self.window, text="9", command=lambda: self.set_text("9", self.window.focus_get()))
+        input_clear = tk.Button(self.window, text="Clear", command=lambda: self.set_text("", self.window.focus_get()))
         reset_button.place(relx=0.5, rely=0.95, anchor='center')
         submit_button.place(relx=0.5, rely=0.87, anchor='center')
-
-        input_one = tk.Button(self.window, text="1", command=lambda: self.set_text("1", self.window.focus_get()))
         input_one.place(relx=0.75, rely=0.25)
-        input_two = tk.Button(self.window, text="2", command=lambda: self.set_text("2", self.window.focus_get()))
         input_two.place(relx=0.83, rely=0.25)
-        input_three = tk.Button(self.window, text="3", command=lambda: self.set_text("3", self.window.focus_get()))
         input_three.place(relx=0.91, rely=0.25)
-        input_four = tk.Button(self.window, text="4", command=lambda: self.set_text("4", self.window.focus_get()))
         input_four.place(relx=0.75, rely=0.15)
-        input_five = tk.Button(self.window, text="5", command=lambda: self.set_text("5", self.window.focus_get()))
         input_five.place(relx=0.83, rely=0.15)
-        input_six = tk.Button(self.window, text="6", command=lambda: self.set_text("6", self.window.focus_get()))
         input_six.place(relx=0.91, rely=0.15)
-        input_seven = tk.Button(self.window, text="7", command=lambda: self.set_text("7", self.window.focus_get()))
         input_seven.place(relx=0.75, rely=0.05)
-        input_eight = tk.Button(self.window, text="8", command=lambda: self.set_text("8", self.window.focus_get()))
         input_eight.place(relx=0.83, rely=0.05)
-        input_nine = tk.Button(self.window, text="9", command=lambda: self.set_text("9", self.window.focus_get()))
         input_nine.place(relx=0.91, rely=0.05)
-        input_clear = tk.Button(self.window, text="Clear", command=lambda: self.set_text("", self.window.focus_get()))
         input_clear.place(relx=0.81, rely=0.35)
 
-    def set_text(self, text, entry):
-        entry.input_value(text)
+    # This applies button value to focused entry
+    def set_text(self, text, cell):
+        cell.input_value(text)
         return
 
-    # TODO: Refactor this. Make a win function-> ask for name surname then save sudoku solution in .txt
-    def all_answers(self):
+    # Checks if cells are correctly filled. If any single one isn't, the game is not over yet.
+    def win_check(self):
         for cell in self.input_fields:
             if not cell.solved():
                 fail_button = tk.Button(self.window, text="The solution does not fit. Try again :)",
                                         command=lambda: fail_button.place_forget())
                 fail_button.place(relx=0.5, rely=0.5, anchor='center')
                 return False
-        self.won_game()
+        self.won_game_window()
 
-    def won_game(self):
-        self.newWindow = tk.Toplevel(self.window)
-        self.newWindow.title("You Solved It!")
-        self.newWindow.geometry("200x120")
+    # Opens up a new window in which we can input name and surname of the player
+    def won_game_window(self):
+        self.save_game_window = tk.Toplevel(self.window)
+        self.save_game_window.title("You Solved It!")
+        self.save_game_window.geometry("200x120")
         name_var = tk.StringVar()
         surname_var = tk.StringVar()
-        name_label = tk.Label(self.newWindow, text='Name', font=('calibre', 10, 'bold'))
-        name_entry = tk.Entry(self.newWindow, textvariable=name_var, font=('calibre', 10, 'normal'))
-        surname_label = tk.Label(self.newWindow, text='Surname', font=('calibre', 10, 'bold'))
-        surname_entry = tk.Entry(self.newWindow, textvariable=surname_var, font=('calibre', 10, 'normal'))
-        save_btn = tk.Button(self.newWindow, text='Save Solution',
-                             command=lambda: self.save_game_and_create_new(self.newWindow, name_var, surname_var))
+        name_label = tk.Label(self.save_game_window, text='Name', font=('calibre', 10, 'bold'))
+        name_entry = tk.Entry(self.save_game_window, textvariable=name_var, font=('calibre', 10, 'normal'))
+        surname_label = tk.Label(self.save_game_window, text='Surname', font=('calibre', 10, 'bold'))
+        surname_entry = tk.Entry(self.save_game_window, textvariable=surname_var, font=('calibre', 10, 'normal'))
+        save_btn = tk.Button(self.save_game_window, text='Save Solution',
+                             command=lambda: self.won_game(self.save_game_window, name_var, surname_var))
         name_label.pack()
         name_entry.pack()
         surname_label.pack()
         surname_entry.pack()
         save_btn.pack()
 
-    def save_game_and_create_new(self, top, name, surname):
-        # save to db name_surname.txt
+    # Save the game, close the window and start a new game
+    def won_game(self, save_game_window, name, surname):
+        self.save_game(name, surname)
+        save_game_window.destroy()
+        save_game_window.update()
+        self.new_game()
+
+    # Create a file with format name_surname.txt that contains the board state
+    def save_game(self, name, surname):
         file_name = name.get() + '_' + surname.get() + '.txt'
         if file_name == '_.txt':
             file_name = 'solution.txt'
         f = open('./db/' + file_name, 'w')
         f.write(np.array2string(self.current_state))
         f.close()
-        # close new window
-        top.destroy()
-        top.update()
-        # Make a new game
-        pass
 
+    # We use this to figure out which sudoku square (matrix 3x3)  of the grid we are in
     @staticmethod
     def roundup_to_nearest_three(index):
         roundup_float = np.ceil((index + 1) / 3) * 3
         roundup_int = np.int(roundup_float)
         return roundup_int
 
+    # We can't use the same value twice in a row column or sudoku square, so here we check for what has already
+    # been used up
     def used_values(self, row, column):
         row_values = np.unique(self.current_state[row, :])
         col_values = np.unique(self.current_state[:, column])
@@ -128,3 +140,7 @@ class Sudoku:
         if used_values[0] == '':
             used_values = np.delete(used_values, 0)
         return used_values
+
+    # We update the current state so we can validate inputs
+    def update_state(self, row, column, new_value):
+        self.current_state[row][column] = new_value
--- GameUI/SudokuCell.py
+++ GameUI/SudokuCell.py
@@ -1,23 +1,20 @@
 import tkinter as tk
-# We have a sudoku container
-# We have sudokuCells
-# an invalid input would be checked on sudoku cell level
-# to get it we need to track current sudoku status
-# Row - column must be 1 to 9 and each square should have it. To check this we have to keep track of current inputs
 
-
+# Individual cell of the grid
 class SudokuCell(tk.Entry):
-    def __init__(self, sudoku, row, column, expected_number, master=None, **kwargs):
+    def __init__(self, sudoku, row, column, expected_value, master=None, **kwargs):
+        self.expected_value = expected_value
         self.sudoku = sudoku
         self.row = row
         self.column = column
         self.var = tk.StringVar()
-        tk.Entry.__init__(self, master, textvariable=self.var, **kwargs)
-        self.expected_value = expected_number
+        tk.Entry.__init__(self, master, textvariable=self.var, justify='center', **kwargs)
         self.old_value = ''
         self.var.trace('w', self.check)
         self.get, self.set = self.var.get, self.var.set
 
+    # Make sure that what we are inputting is a digit. If its 0 clear the field
+    # If the value is used up then display the text as red
     def check(self, *args):
         input_value = self.get().strip()
         input_value = self.leading_char_added(input_value)
@@ -38,7 +35,10 @@ class SudokuCell(tk.Entry):
                 self.configure(foreground='Red')
             else:
                 self.configure(foreground='Black')
+        self.sudoku.update_state(self.row, self.column, self.get())
 
+    # We have to check if the user inputted a char as leading. Exmpl: If input was '2', and the user inputs '1' such
+    # that the new input is '12'
     def leading_char_added(self, input_value):
         if input_value == '':
             return ''
@@ -48,9 +48,11 @@ class SudokuCell(tk.Entry):
             input_value = input_value[-2]
         return input_value
 
-    def input_value(self,new_value):
+    # This allows buttons to input values into sudoku cells
+    def input_value(self, new_value):
         self.old_value = new_value
         self.set(new_value)
 
+    # Check if current input is equal to expected input
     def solved(self):
-        return self.get().strip() == str(self.expected_value)
+        return self.get() == self.expected_value
